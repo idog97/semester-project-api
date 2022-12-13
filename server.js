7dd75250454361ac8db9108c818baf8dbf4daf15
Ido Garala <idoga207@gmail.com>
2022-12-13 20:15:51 +0200

    env addition and workarounds

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+//הפעלת פוקנציה מתוך ספריית אי אן וי שטוענת את ההגדרות מקובץ אי אן וי
+require('dotenv').config();
+function SendEmail(u,p)
+{
+    console.log("sent Email to "+ u);
+    //procss = אובייקט שמייצג את האפליקציה שלי
+    //קרא לגוגל והתחבר באמצעות השם והסיסמה
+}
+SendEmail(process.env.USER_EMAIL,process.env.EMAIL_PASS);
 const http = require('http');
 const app = require('./app');
 const port  = 5005;
