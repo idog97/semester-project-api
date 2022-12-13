7276035d622564e3f7dd7f784e1dec93c2072e8d
Ido Garala <idoga207@gmail.com>
2022-12-13 19:10:18 +0200

    correcting bugs of server

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const morgan = require('morgan');
 const Productrouter = require('./api/v1/routes/product')
 app.use(morgan('common'));
 app.use("/product",Productrouter);
-
+//סתם הערה
 module.exports = app;
