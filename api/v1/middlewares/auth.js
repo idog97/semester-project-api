da1e154aa3f32f5838e5abf1d9f8e5d7d3bbc672
Ido Garala <idoga207@gmail.com>
2022-12-27 18:05:29 +0200

    crud addition

--- api/v1/middlewares/auth.js
+++ api/v1/middlewares/auth.js
@@ -1,5 +1,22 @@
-function Auth(req,res,next)
-{
-    console.log(req.url);
-    next();
-}
+module.exports=((req,res,next)=>{
+    const myIP = req.socket.remoteAddress;
+    const arr=[192.168,123.55,43.44,123.44,myIP];
+    let count =0;
+    for(let i=0; i<arr.length; i++)
+    {
+        if(arr[i]==myIP)
+        {
+            count++;
+            next();
+        }
+    }
+
+    if(count>0)
+    {
+        return res.status(200).json({msg:"my ip address is: " + myIP + " is found"});
+    }
+    else
+    {
+        return res.status(404).json({msg:"your ip is not found"});
+    }
+});
